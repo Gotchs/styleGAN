dec67e6992f016a19d8c4720ce4faf0c72b0681d
Hongyuan Du <du-hongyuan@outlook.com>
2018-11-15 23:44:30 -0800

    Add InstanceNorm networks.

--- models/fixed_networks.py
+++ models/fixed_networks.py
@@ -29,6 +29,7 @@ def dc_Gen_R_BN():
         nn.Tanh()
     )
 
+
 def dc_Dis_LR_BN():
     '''
     Fixed implementation of DCGAN discriminator
@@ -52,8 +53,59 @@ def dc_Dis_LR_BN():
         nn.Sigmoid()
     )
 
+
+def dc_Gen_IN_R():
+    '''
+    Fixed implementation of DCGAN generator
+    Relu after InstanceNorm
+    Structure reference: https://arxiv.org/abs/1511.06434
+    '''
+    return nn.Sequential(
+        nn.Linear(100, 1024 * 4 * 4),
+        nn.InstanceNorm1d(1024 * 4 * 4),
+        nn.ReLU(inplace=True),
+        mutils.Unflatten(-1, 1024, 4, 4),
+        nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=5, stride=2, padding=2, output_padding=1),
+        nn.InstanceNorm2d(512),
+        nn.ReLU(inplace=True),
+        nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=5, stride=2, padding=2, output_padding=1),
+        nn.InstanceNorm2d(256),
+        nn.ReLU(inplace=True),
+        nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=5, stride=2, padding=2, output_padding=1),
+        nn.InstanceNorm2d(128),
+        nn.ReLU(inplace=True),
+        nn.ConvTranspose2d(in_channels=128, out_channels=3, kernel_size=5, stride=2, padding=2, output_padding=1),
+        nn.Tanh()
+    )
+
+
+def dc_Dis_IN_LR():
+    '''
+    Fixed implementation of DCGAN discriminator
+    LeakyRelu after InstanceNorm
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=2, padding=2),
+        nn.InstanceNorm2d(64),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=2),
+        nn.InstanceNorm2d(128),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=2, padding=2),
+        nn.InstanceNorm2d(256),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=5, stride=2, padding=2),
+        nn.InstanceNorm2d(512),
+        nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        mutils.Flatten(),
+        nn.Linear(512 * 4 * 4, 1),
+        nn.Sigmoid()
+    )
+
+
 def ls_Gen_IN_R():
     pass
+
 
 def ls_Dis_IN_LR():
     pass
