f3d37168b859dbfc9dbc4fa8e98444c4e1fbca23
Hongyuan Du <du-hongyuan@outlook.com>
2018-11-29 13:45:20 -0800

    Add weight init methods and script for training LSGAN_Res

--- models/fixed_GANs.py
+++ models/fixed_GANs.py
@@ -85,3 +85,48 @@ class LSGAN_Res(BaseGAN):
         self.get_dataset(dset_name=ckp['dset_name'], classes=[ckp['classes']])
         self.get_dataloader(ckp['batch_size'])
         self.get_loss(loss_name=ckp['loss_name'], soft_label=ckp['soft_label'])
+
+
+    def init_weights(self, init_type='kaiming', gain=0.02):
+        
+        def init_func_G(m):
+            classname = m.__class__.__name__
+            if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+                if init_type == 'normal':
+                    init.normal_(m.weight.data, 0.0, gain)
+                elif init_type == 'xavier':
+                    init.xavier_normal_(m.weight.data, gain=gain)
+                elif init_type == 'kaiming':
+                    init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+                elif init_type == 'orthogonal':
+                    init.orthogonal_(m.weight.data, gain=gain)
+                else:
+                    raise NotImplementedError('Initialization method [%s] is not implemented' % init_type)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    init.constant_(m.bias.data, 0.0)
+            elif classname.find('BatchNorm2d') != -1:
+                init.normal_(m.weight.data, 1.0, gain)
+                init.constant_(m.bias.data, 0.0)
+
+        def init_func_D(m):
+            classname = m.__class__.__name__
+            if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+                if init_type == 'normal':
+                    init.normal_(m.weight.data, 0.0, gain)
+                elif init_type == 'xavier':
+                    init.xavier_normal_(m.weight.data, gain=gain)
+                elif init_type == 'kaiming':
+                    init.kaiming_normal_(m.weight.data, a=0.2, mode='fan_in')
+                elif init_type == 'orthogonal':
+                    init.orthogonal_(m.weight.data, gain=gain)
+                else:
+                    raise NotImplementedError('Initialization method [%s] is not implemented' % init_type)
+                if hasattr(m, 'bias') and m.bias is not None:
+                    init.constant_(m.bias.data, 0.0)
+            elif classname.find('BatchNorm2d') != -1:
+                init.normal_(m.weight.data, 1.0, gain)
+                init.constant_(m.bias.data, 0.0)
+
+        print('Initialize network with %s' % init_type)
+        self.G.apply(init_func_G)
+        self.D.apply(init_func_D)
